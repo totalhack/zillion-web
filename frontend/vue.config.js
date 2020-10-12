2befb6ea3d3b1fbfef801c3474fe6f18f600ea6a
totalhack <none@none.com>
2020-10-12 11:16:12 -0400

    bug: try to fix PWA settings

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -36,4 +36,17 @@ module.exports = {
         }
       }));
   },
+  pwa: {
+    themeColor: '#272727',
+    msTileColor: '#000000',
+    // appleMobileWebAppCapable: 'yes',
+    // appleMobileWebAppStatusBarStyle: 'black',
+    iconPaths: {
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/safari-pinned-tab.svg',
+      msTileImage: 'img/icons/msapplication-icon-144x144.png'
+    }
+  }
 }
