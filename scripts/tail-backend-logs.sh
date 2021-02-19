b67cbfc03941dff795ffce28aef86fe32fd23370
none <none@none.com>
2021-02-19 10:17:52 -0500

    feat: aws log tailing scripts

---
+++ scripts/tail-backend-logs.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env sh
+
+# Exit in case of error
+set -e
+
+awslogs get ${1:-backend} ALL --start='5m' -w
---
+++ scripts/tail-frontend-logs.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/env sh
+
+# Exit in case of error
+set -e
+
+awslogs get ${1:-frontend} ALL --start='5m' -w
