ad4f28accc178a97abdd97bb81184d0cb6f24718
totalhack <none@none.com>
2022-07-24 21:28:47 -0400

    feat: improve log script formats

--- scripts/tail-backend-logs.sh
+++ scripts/tail-backend-logs.sh
@@ -3,4 +3,4 @@
 # Exit in case of error
 set -e
 
-awslogs get ${1:-backend} ALL --start='5m' -w
+awslogs get ${1:-backend} ALL --start='5m' -w -S -G
--- scripts/tail-frontend-logs.sh
+++ scripts/tail-frontend-logs.sh
@@ -3,4 +3,4 @@
 # Exit in case of error
 set -e
 
-awslogs get ${1:-frontend} ALL --start='5m' -w
+awslogs get ${1:-frontend} ALL --start='5m' -w -S -G
